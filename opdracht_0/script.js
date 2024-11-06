2272cd96c75655e5b9bdf828239307dc4f4d686e
YuriiF228 <9026220@student.zadkine.nl>
2024-11-06 13:55:43 +0100

    Opdracht 0

--- opdracht_0/script.js
+++ opdracht_0/script.js
@@ -1,16 +1,21 @@
 let btn = document.querySelector(".verif-button"),
     btnControl = document.getElementById("control"),
-    verifObj = document.getElementById("verifObj"),
-    age = document.getElementById("verif-input").value;
+    verifObj = document.getElementById("verifObj");
 
 btn.onclick = () => {
     verifObj.style.display = "block";
+
 }
-// btnControl.onclick = () => {
-//     if (age <= 18) {
-//         window.location.href = "";
-//     }
-//     else (age => 18) {
-//         window.write = "helaas je bent te jong";
-//     }
-// }
+btnControl.onclick = () => {
+    age = document.getElementById("verif-input").value;
+
+
+    if (isNaN(age) || age > 18) {
+        window.location.href = "https://www.techniekcollegerotterdam.nl/locaties/schiedamseweg";
+    }
+    else {
+        verifObj.style.display = "block";
+        const fout = document.querySelector(".fout");
+        fout.innerHTML = "<h1>Jij bent te jong!!!!</h1>";
+    }
+}
