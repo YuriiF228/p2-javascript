a5c03474e3291135741e5b2b2232230f2ec26ca0
YuriiF228 <9026220@student.zadkine.nl>
2024-11-26 14:05:17 +0100

    Update app.js

--- opdracht_8/app.js
+++ opdracht_8/app.js
@@ -1,19 +1,3 @@
-// let movingDiv = document.getElementById("movingDiv");
-// let currentLeft = parseInt(movingDiv.style.left) || 0;
-// movingDiv.addEventListener('click', () => {
-//     var colors = ['green', 'red', 'blue', 'yellow'];
-//     var randomColor = colors[Math.floor(Math.random() * colors.length)];
-//     movingDiv.style.background = randomColor;
-// });
-
-// movingDiv.addEventListener('keydown', (event) => {
-//     if(event.key == 'd') {
-//         movingDiv.style.left = (currentLeft + 10) + 'px';
-//     }
-// });
-// movingDiv.tabIndex = 0;
-// movingDiv.focus();
-
 
 
 let movingDiv = document.getElementById("movingDiv");
@@ -48,7 +32,7 @@ movingDiv.addEventListener('keydown', (event) => {
     else{
         console.log("error")
     }
-    
+
     if(event.key == 'w') {
         let currentTop = parseInt(movingDiv.style.top) || 0;        
         movingDiv.style.top = (currentTop - 10) + 'px';
