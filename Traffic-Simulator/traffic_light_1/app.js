21b5c38744eaf43e6bb48f3ac071986ed3db7083
YuriiF228 <9026220@student.zadkine.nl>
2024-12-03 10:04:54 +0100

    opdracyt 1

--- Traffic-Simulator/traffic_light_1/app.js
+++ Traffic-Simulator/traffic_light_1/app.js
@@ -1,1 +1,12 @@
+let trafficLight = document.getElementById('traffic-light');
+trafficLight.src = "./trafficLightRed.png";
 
+trafficLight.addEventListener('click', () => {
+    if (trafficLight.src.includes("Red")) {
+        trafficLight.src = "./trafficLightYellow.png";
+    } else if (trafficLight.src.includes("Yellow")) {
+        trafficLight.src = "./trafficLightGreen.png";
+    } else {
+        trafficLight.src = "./trafficLightRed.png";
+    }
+});
